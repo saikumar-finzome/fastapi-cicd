734a0f67e65272c976cd9efafe2649239c975489
saikumar <sai999694@gmail.com>
2022-09-10 11:56:48 +0530

    test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,16 @@
 from fastapi.testclient import TestClient
-from ..main import app
+from app.main import app
 
 client = TestClient(app)
 
 
 def test_main_resource():
-    response_auth = client.get(f"/")
+    response_auth = client.get("/")
     assert response_auth.status_code == 200
-
 
 def test_child_resource():
-    response_auth = client.get(f"/api/v1/test")
+    response_auth = client.get("/api/v1/test")
     assert response_auth.status_code == 200
+
+
+    
